7d70a46793e9e611817a72ce06c87e7161370a2f
shugetsu <1968304304@qq.com>
2021-12-06 13:10:55 +0800

    test: ci-26

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-06 10:17:44
+ * @LastEditTime: 2021-12-06 13:10:44
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \v-chat-io\src\app.service.ts
+ */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World 凉!';
   }
 }
